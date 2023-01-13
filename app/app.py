9898db9431fe0891ebc21eec78f556764e7610f0
Chouki Gueddar <98185347+Janna2020@users.noreply.github.com>
2023-01-13 10:32:53 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,11 +1,6 @@
-from tkinter.tix import IMAGE
+
 import streamlit as st
 import pandas as pd
 import numpy as np
-from PIL import Image
 
 st.title('STAR WARS')
-
-video_file = open('Video_star.mov', 'rb')
-video_bytes = video_file.read()
-st.video(video_bytes)
